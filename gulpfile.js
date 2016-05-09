8c1e3ce630c6e143b9c41f7b1da321b7d37a040f
ivan <ivan@ivan-Aspire-VN7-791>
2016-05-09 12:34:55 +0200

    readme

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ var jshint = require('gulp-jshint');
 
 // Test project directory
 var testProjectDirectory1 = '/home/ivan/NetBeansProjects/com.successful.bablo/www';
-var testProjectDirectory2 = '/home/ivan/NetBeansProjects/app-ionicsettings/www';
+var testProjectDirectory2 = '/home/ivan/Dropbox/Arbeit/ionic-settings/example/www';
 
 // Root directory
 var rootDirectory = path.resolve('./');
